a6467013c3bdb3adcd82f1e69adbc118ab956425
Mykhailo Nester <nsmisha.dev@gmail.com>
2019-02-04 21:49:21 +0200

    - updated TabItems dynamically and add new tab; - update resources;

--- example/src/main/java/com/crowdin/platform/example/fragments/SlideshowFragment.java
+++ example/src/main/java/com/crowdin/platform/example/fragments/SlideshowFragment.java
@@ -2,12 +2,16 @@ package com.crowdin.platform.example.fragments;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.crowdin.platform.example.R;
+
+import java.util.Objects;
 
 public class SlideshowFragment extends Fragment {
 
@@ -18,5 +22,17 @@ public class SlideshowFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_slideshow, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        TabLayout tabLayout = view.findViewById(R.id.tab_layout);
+
+        final String[] titles = getResources().getStringArray(R.array.tab_array);
+        for (int i = 0; i < tabLayout.getTabCount(); i++) {
+            Objects.requireNonNull(tabLayout.getTabAt(i)).setText(titles[i]);
+        }
+
+        tabLayout.addTab(tabLayout.newTab().setText(getString(R.string.dynamic_tab_title)));
     }
 }
