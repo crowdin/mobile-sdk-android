f7bde71e70cb279695ab9a1483d279291f306414
Mykhailo Nester <nsmisha.dev@gmail.com>
2019-04-06 10:54:02 +0300

    - fix NPE when network error while requesting;

--- crowdin/src/main/java/com/crowdin/platform/repository/remote/DefaultRemoteRepository.kt
+++ crowdin/src/main/java/com/crowdin/platform/repository/remote/DefaultRemoteRepository.kt
@@ -3,6 +3,10 @@ package com.crowdin.platform.repository.remote
 import com.crowdin.platform.repository.LanguageDataCallback
 import com.crowdin.platform.repository.parser.Reader
 import com.crowdin.platform.repository.remote.api.CrowdinApi
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import java.net.HttpURLConnection
 
 internal class DefaultRemoteRepository(private val crowdinApi: CrowdinApi,
@@ -11,12 +15,20 @@ internal class DefaultRemoteRepository(private val crowdinApi: CrowdinApi,
     override fun fetchData(distributionKey: String?, currentLocale: String, filePath: String, languageDataCallback: LanguageDataCallback) {
         if (distributionKey == null) return
 
-        val response = crowdinApi.getFileUpdates(distributionKey, currentLocale, filePath).execute()
-        val body = response.body()
-        if (response.code() == HttpURLConnection.HTTP_OK && body != null) {
-            val languageData = reader.parseInput(body.byteStream())
-            languageData.language = currentLocale
-            languageDataCallback.onDataLoaded(languageData)
-        }
+        crowdinApi.getFileUpdates(distributionKey, currentLocale, filePath)
+                .enqueue(object : Callback<ResponseBody> {
+
+                    override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>) {
+                        val body = response.body()
+                        if (response.code() == HttpURLConnection.HTTP_OK && body != null) {
+                            val languageData = reader.parseInput(body.byteStream())
+                            languageData.language = currentLocale
+                            languageDataCallback.onDataLoaded(languageData)
+                        }
+                    }
+
+                    override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
+                    }
+                })
     }
 }
