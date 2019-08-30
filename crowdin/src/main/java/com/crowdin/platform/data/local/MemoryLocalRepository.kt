c5596f575c089195c0c65e3c55dcf9a859490e72
Mykhailo Nester <nsmisha.dev@gmail.com>
2019-08-30 21:30:52 +0300

    fix IllegalConversion exception;

--- crowdin/src/main/java/com/crowdin/platform/data/local/MemoryLocalRepository.kt
+++ crowdin/src/main/java/com/crowdin/platform/data/local/MemoryLocalRepository.kt
@@ -142,12 +142,16 @@ internal class MemoryLocalRepository : LocalRepository {
         languageData?.plurals?.forEach { pluralData ->
             val pluralName = pluralData.name
             pluralData.quantity.forEach {
-                if (it.value == text ||
-                        (pluralData.formatArgs.isNotEmpty()
-                                && it.value == String.format(text, pluralData.formatArgs))) {
-                    searchResultData.pluralName = pluralName
-                    searchResultData.pluralQuantity = pluralData.number
-                    searchResultData.pluralFormatArgs = pluralData.formatArgs
+                try {
+                    if (it.value == text ||
+                            (pluralData.formatArgs.isNotEmpty()
+                                    && it.value == String.format(text, pluralData.formatArgs))) {
+                        searchResultData.pluralName = pluralName
+                        searchResultData.pluralQuantity = pluralData.number
+                        searchResultData.pluralFormatArgs = pluralData.formatArgs
+                        return
+                    }
+                } catch (ex: IllegalFormatConversionException) {
                     return
                 }
             }
