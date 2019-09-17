abaabf6bb9e01397a2deacd5b4c498ea3e0a557c
nmyhalchuk <nazar.mykhalchuk@crowdin.com>
2019-09-17 11:43:39 +0300

    ADD support export pattern language, two_letter_code, three_letters_code

--- crowdin/src/main/java/com/crowdin/platform/data/remote/BaseRepository.kt
+++ crowdin/src/main/java/com/crowdin/platform/data/remote/BaseRepository.kt
@@ -7,6 +7,9 @@ internal abstract class BaseRepository : RemoteRepository {
     internal companion object {
         const val HEADER_ETAG = "ETag"
         const val HEADER_ETAG_EMPTY = ""
+        const val LANGUAGE_NAME = "%language%"
+        const val TWO_LETTER_CODE = "%two_letters_code%"
+        const val THREE_LETTER_CODE = "%three_letters_code%"
         const val LOCALE = "%locale%"
         const val LOCALE_WITH_UNDERSCORE = "%locale_with_underscore%"
         const val ANDROID_CODE = "%android_code%"
@@ -18,9 +21,14 @@ internal abstract class BaseRepository : RemoteRepository {
         var path = filePath
         val locale = Locale.getDefault()
         val language = locale.language
+        val languageThreeLetterCode = locale.isO3Language
+        val languageName = Locale.getDefault().getDisplayLanguage(Locale.ENGLISH)
         val country = locale.country
 
         when {
+            path.contains(LANGUAGE_NAME) -> path = path.replace(LANGUAGE_NAME, "$languageName")
+            path.contains(TWO_LETTER_CODE) -> path = path.replace(TWO_LETTER_CODE, "$language")
+            path.contains(THREE_LETTER_CODE) -> path = path.replace(THREE_LETTER_CODE, "$languageThreeLetterCode")
             path.contains(LOCALE) -> path = path.replace(LOCALE, "$language-$country")
             path.contains(LOCALE_WITH_UNDERSCORE) -> path = path.replace(LOCALE_WITH_UNDERSCORE, locale.toString())
             path.contains(ANDROID_CODE) -> path = path.replace(ANDROID_CODE, "$language-r$country")
