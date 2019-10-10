070ddf6b96917a386c29fcb33b0526b5816693cf
Mykhailo Nester <nsmisha.dev@gmail.com>
2019-10-10 23:00:27 +0300

    add test for feature flag class;

---
+++ crowdin/src/test/java/com/crowdin/platform/FeatureFlagTest.kt
@@ -0,0 +1,37 @@
+package com.crowdin.platform
+
+import com.crowdin.platform.util.FeatureFlags
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.core.Is.`is`
+import org.junit.Test
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.mock
+
+class FeatureFlagTest {
+
+    @Test
+    fun whenSetRealtimeUpdatesEnabled_shouldPersistDuringSession() {
+        // Given
+        val config = mock(CrowdinConfig::class.java)
+        `when`(config.isRealTimeUpdateEnabled).thenReturn(true)
+
+        // When
+        FeatureFlags.registerConfig(config)
+
+        // Then
+        assertThat(FeatureFlags.isRealTimeUpdateEnabled, `is`(true))
+    }
+
+    @Test
+    fun whenSetScreenshotsEnabled_shouldPersistDuringSession() {
+        // Given
+        val config = mock(CrowdinConfig::class.java)
+        `when`(config.isScreenshotEnabled).thenReturn(true)
+
+        // When
+        FeatureFlags.registerConfig(config)
+
+        // Then
+        assertThat(FeatureFlags.isScreenshotEnabled, `is`(true))
+    }
+}
