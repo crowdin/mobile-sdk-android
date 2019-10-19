20a10b7699bdeb0c4ece8cb38d63aa1967b3d6a6
Mykhailo Nester <nsmisha.dev@gmail.com>
2019-10-19 14:24:57 +0300

    add tests for session interceptor class;

--- crowdin/src/test/java/com/crowdin/platform/SessionInterceptorTest.kt
+++ crowdin/src/test/java/com/crowdin/platform/SessionInterceptorTest.kt
@@ -1,37 +1,161 @@
 package com.crowdin.platform
 
+import android.content.Context
+import android.content.SharedPreferences
+import android.net.ConnectivityManager
 import com.crowdin.platform.data.remote.SessionInterceptor
 import okhttp3.Interceptor
 import okhttp3.Request
 import okhttp3.Response
+import org.junit.Before
 import org.junit.Test
+import org.mockito.ArgumentMatchers.anyInt
+import org.mockito.ArgumentMatchers.anyString
 import org.mockito.Mockito.*
 
 class SessionInterceptorTest {
 
-    // TODO: update
+    @Before
+    fun setUp() {
+        initCrowdin()
+    }
+
     @Test
-    fun intercept_whenTokenExpired_shouldRefreshToken() {
+    fun intercept_whenIntercept_shouldCheckTokenExpiration() {
         // Given
-        val session = mock(Session::class.java)
+        val session = givenMockSession()
         val sessionInterceptor = SessionInterceptor(session)
-        val chain = mock(Interceptor.Chain::class.java)
-        `when`(session.isTokenExpired()).thenReturn(false)
-//        `when`(session.refreshToken(any())).thenReturn(true)
-        val request = mock(Request::class.java)
-        `when`(chain.request()).thenReturn(request)
-        `when`(session.getAccessToken()).thenReturn("testAccessToken")
-        val builder = mock(Request.Builder::class.java)
-        `when`(request.newBuilder()).thenReturn(builder)
-        `when`(builder.build()).thenReturn(request)
-        val response = mock(Response::class.java)
-        `when`(chain.proceed(any())).thenReturn(response)
+        val chain = givenMockChain()
 
         // When
         sessionInterceptor.intercept(chain)
 
         // Then
         verify(session).isTokenExpired()
-        verify(session).getAccessToken()
+    }
+
+    @Test
+    fun intercept_whenTokenExpired_shouldTryRefresh() {
+        // Given
+        val session = givenMockSession()
+        `when`(session.isTokenExpired()).thenReturn(true)
+        val sessionInterceptor = SessionInterceptor(session)
+        val chain = givenMockChain()
+
+        // When
+        sessionInterceptor.intercept(chain)
+
+        // Then
+        verify(session).refreshToken(any())
+    }
+
+    @Test
+    fun intercept_whenTokenExpiredAndRefreshFailed_shouldInvalidate() {
+        // Given
+        val session = givenMockSession()
+        `when`(session.isTokenExpired()).thenReturn(true)
+        `when`(session.refreshToken(any())).thenReturn(false)
+        val sessionInterceptor = SessionInterceptor(session)
+        val chain = givenMockChain()
+
+        // When
+        sessionInterceptor.intercept(chain)
+
+        // Then
+        verify(session).invalidate()
+    }
+
+
+    @Test
+    fun intercept_whenAuthError_shouldTryToRefreshToken() {
+        // Given
+        initCrowdin()
+        val session = givenMockSession()
+        `when`(session.isTokenExpired()).thenReturn(false)
+        `when`(session.refreshToken(any())).thenReturn(false)
+        val sessionInterceptor = SessionInterceptor(session)
+        val chain = givenMockChain()
+        givenFailResponse(chain)
+
+        // When
+        sessionInterceptor.intercept(chain)
+
+        // Then
+        verify(session).refreshToken(any())
+    }
+
+    @Test
+    fun intercept_whenAuthErrorAndRefreshFailed_shouldInvalidate() {
+        // Given
+        initCrowdin()
+        val session = givenMockSession()
+        `when`(session.isTokenExpired()).thenReturn(false)
+        `when`(session.refreshToken(any())).thenReturn(false)
+        val sessionInterceptor = SessionInterceptor(session)
+        val chain = givenMockChain()
+        givenFailResponse(chain)
+
+        // When
+        sessionInterceptor.intercept(chain)
+
+        // Then
+        verify(session).invalidate()
+    }
+
+    @Test
+    fun intercept_whenAuthErrorAndRefreshSuccess_shouldRetryRequestSilently() {
+        // Given
+        initCrowdin()
+        val session = givenMockSession()
+        `when`(session.isTokenExpired()).thenReturn(false)
+        `when`(session.refreshToken(any())).thenReturn(true)
+        val sessionInterceptor = SessionInterceptor(session)
+        val chain = givenMockChain()
+        givenFailResponse(chain)
+
+        // When
+        sessionInterceptor.intercept(chain)
+
+        // Then
+        verify(session, times(2)).getAccessToken()
+    }
+
+    private fun initCrowdin() {
+        val config = CrowdinConfig.Builder()
+                .withFilePaths("test")
+                .withDistributionHash("test")
+                .build()
+        val sharedPrefs = mock(SharedPreferences::class.java)!!
+        val context = mock(Context::class.java)
+        `when`(context.getSharedPreferences(anyString(), anyInt())).thenReturn(sharedPrefs)
+        val connectivityManager = mock(ConnectivityManager::class.java)
+        `when`(context.getSystemService(Context.CONNECTIVITY_SERVICE)).thenReturn(connectivityManager)
+
+        Crowdin.init(context, config)
+    }
+
+    private fun givenMockChain(): Interceptor.Chain {
+        val chain = mock(Interceptor.Chain::class.java)
+        val request = mock(Request::class.java)
+        `when`(chain.request()).thenReturn(request)
+        val builder = mock(Request.Builder::class.java)
+        `when`(request.newBuilder()).thenReturn(builder)
+        `when`(builder.build()).thenReturn(request)
+        val response = mock(Response::class.java)
+        `when`(chain.proceed(any())).thenReturn(response)
+
+        return chain
+    }
+
+    private fun givenMockSession(): Session {
+        val session = mock(Session::class.java)
+        `when`(session.getAccessToken()).thenReturn("testAccessToken")
+        return session
+    }
+
+    private fun givenFailResponse(chain: Interceptor.Chain) {
+        val response = mock(Response::class.java)
+        `when`(chain.proceed(any())).thenReturn(response)
+        `when`(response.code).thenReturn(401)
     }
 }
