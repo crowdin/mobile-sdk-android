f8560e357bf8f4e8bc62f0205d7fe6ff3bc2cb28
Mykhailo Nester <nsmisha.dev@gmail.com>
2019-10-27 21:03:34 +0200

    - add tests for recurring manager class;

---
+++ crowdin/src/test/java/com/crowdin/platform/RecurringManagerTest.kt
@@ -0,0 +1,35 @@
+package com.crowdin.platform
+
+import android.content.Context
+import android.content.SharedPreferences
+import com.crowdin.platform.recurringwork.RecurringManager
+import com.google.gson.Gson
+import org.hamcrest.CoreMatchers.instanceOf
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Test
+import org.mockito.ArgumentMatchers
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.mock
+
+class RecurringManagerTest {
+
+    @Test
+    fun whenSetPeriodicUpdates_shouldUpdatePref() {
+        // Given
+        val config = CrowdinConfig.Builder()
+                .withFilePaths("test")
+                .withDistributionHash("test")
+                .build()
+        val json = Gson().toJson(config)
+        val sharedPrefs = mock(SharedPreferences::class.java)!!
+        `when`(sharedPrefs.getString("crowdin_config", "")).thenReturn(json)
+        val context = mock(Context::class.java)
+        `when`(context.getSharedPreferences(ArgumentMatchers.anyString(), ArgumentMatchers.anyInt())).thenReturn(sharedPrefs)
+
+        // When
+        val actualConfig = RecurringManager.getConfig(context)
+
+        // Then
+        assertThat(actualConfig, instanceOf(CrowdinConfig::class.java))
+    }
+}
