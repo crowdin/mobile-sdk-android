46e48bc1a6f3971810d9bf018600c8451eec7f48
Mykhailo Nester <nsmisha.dev@gmail.com>
2020-02-16 00:31:35 +0200

    - add user agent header to all API requests;

---
+++ crowdin/src/main/java/com/crowdin/platform/data/remote/interceptor/HeaderInterceptor.kt
@@ -0,0 +1,29 @@
+package com.crowdin.platform.data.remote.interceptor
+
+import android.os.Build
+import com.crowdin.platform.BuildConfig
+import java.io.IOException
+import okhttp3.Interceptor
+import okhttp3.Request
+import okhttp3.Response
+
+class HeaderInterceptor : Interceptor {
+
+    private val userAgent =
+        "crowdin-android-sdk/${BuildConfig.VERSION_NAME} android/${Build.VERSION.SDK_INT}"
+
+    @Throws(IOException::class)
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val request = addHeadersToRequest(chain.request())
+
+        return chain.proceed(request)
+    }
+
+    private fun addHeadersToRequest(original: Request): Request {
+        val requestBuilder = original.newBuilder()
+            .header("User-Agent", userAgent)
+            .method(original.method, original.body)
+
+        return requestBuilder.build()
+    }
+}
