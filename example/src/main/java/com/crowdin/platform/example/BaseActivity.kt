204689df8999b1bb38b278f5787c34238e3e7ddc
Mykhailo Nester <nsmisha.dev@gmail.com>
2020-10-26 15:38:59 +0200

    - add workaround for issue with new appCompat version where sdk not loading resources from crowdin; - update appcompat/constraint layout versions;

--- example/src/main/java/com/crowdin/platform/example/BaseActivity.kt
+++ example/src/main/java/com/crowdin/platform/example/BaseActivity.kt
@@ -1,21 +1,20 @@
 package com.crowdin.platform.example
 
-import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.BaseContextWrappingDelegate
 import com.crowdin.crowdin_controls.destroyCrowdinControl
 import com.crowdin.crowdin_controls.initCrowdinControl
-import com.crowdin.platform.Crowdin
 
 abstract class BaseActivity : AppCompatActivity() {
 
     /**
      * We should wrap the base context of our activities, which is better to put it
      * on BaseActivity, so that we don't have to repeat it for all activities one-by-one.
+     *
+     * @see BaseContextWrappingDelegate.attachBaseContext2
      */
-    override fun attachBaseContext(newBase: Context) {
-        super.attachBaseContext(Crowdin.wrapContext(newBase))
-    }
+    override fun getDelegate() = BaseContextWrappingDelegate(super.getDelegate())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
