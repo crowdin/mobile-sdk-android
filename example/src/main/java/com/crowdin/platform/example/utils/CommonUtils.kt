18e0cf9b8ec10ecf842541719ee4ea30af88d0f3
Mykhailo Nester <nsmisha.dev@gmail.com>
2021-01-30 18:48:46 +0200

    [#75] SDK breaks styles;
    SDK changes:
    - pass original LayoutInflator when creating copy of existing object. It will apply new context functionality as `Resources` but won't change app theme behavior;
    App changes:
    - add material dialogs;
    - update theme styles and colors;

--- example/src/main/java/com/crowdin/platform/example/utils/CommonUtils.kt
+++ example/src/main/java/com/crowdin/platform/example/utils/CommonUtils.kt
@@ -1,6 +1,5 @@
 package com.crowdin.platform.example.utils
 
-import android.app.AlertDialog
 import android.content.Context
 import android.content.res.Resources
 import android.util.DisplayMetrics
@@ -13,6 +12,7 @@ import com.crowdin.platform.example.category.CategoryDelete
 import com.crowdin.platform.example.category.CategoryUpdate
 import com.crowdin.platform.example.category.DBManagerCategory
 import com.crowdin.platform.util.getLocaleForLanguageCode
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import java.text.ParseException
 import java.text.SimpleDateFormat
 import java.util.Locale
@@ -57,31 +57,28 @@ fun getFormatTime(inputTime: String): String {
  */
 fun dialogAddCategory(context: Context, categoryAdd: CategoryAdd) {
     val li = LayoutInflater.from(context)
-    val promptsView = li.inflate(R.layout.alert_dialog_add_category, null)
+    val promptsView = li.inflate(R.layout.alert_dialog_with_input, null)
+    val input = promptsView.findViewById(R.id.input_et) as EditText
 
-    val alert = AlertDialog.Builder(context)
-    alert.setView(promptsView)
-    val input = promptsView.findViewById(R.id.edtAddCat) as EditText
-    alert.setPositiveButton(R.string.add) { _, _ -> }
-    alert.setNegativeButton(R.string.cancel) { _, _ -> }
-    val alertDialog = alert.create()
-
-    alertDialog.setOnShowListener {
-        val button = alertDialog.getButton(AlertDialog.BUTTON_POSITIVE)
-        button.setOnClickListener {
+    MaterialAlertDialogBuilder(context)
+        .setTitle(context.getString(R.string.add_category))
+        .setView(promptsView)
+        .setPositiveButton(R.string.add) { _, _ ->
             val categoryName: String = input.text.toString().trim()
             if (categoryName != "") {
                 val dbManager = DBManagerCategory(context)
                 dbManager.insert(categoryName)
                 categoryAdd.onCategoryAdded()
-                alertDialog.dismiss()
+
             } else {
-                toastMessage(context, context.getString(R.string.please_enter_category_to_add))
+                toastMessage(
+                    context,
+                    context.getString(R.string.please_enter_category_to_add)
+                )
             }
         }
-    }
-
-    alertDialog.show()
+        .setNegativeButton(R.string.cancel) { _, _ -> }
+        .show()
 }
 
 /**
@@ -91,27 +88,21 @@ fun dialogUpdateCategory(context: Context, id: Int, categoryUpdate: CategoryUpda
     val dbManager = DBManagerCategory(context)
     val catName = dbManager.getCategoryName(id)
     val li = LayoutInflater.from(context)
-    val promptsView = li.inflate(R.layout.alert_dialog_update_category, null)
-    val alert = AlertDialog.Builder(context)
-    alert.setView(promptsView)
-    val input: EditText = promptsView.findViewById(R.id.edtUpdateCat) as EditText
-
+    val promptsView = li.inflate(R.layout.alert_dialog_with_input, null)
+    val input = promptsView.findViewById(R.id.input_et) as EditText
     input.setText(catName)
     input.setSelection(input.text.length)
-    alert.setPositiveButton(R.string.update) { _, _ -> }
-    alert.setNegativeButton(R.string.cancel) { _, _ -> }
-    val alertDialog = alert.create()
 
-    alertDialog.setOnShowListener {
-        val button = alertDialog.getButton(AlertDialog.BUTTON_POSITIVE)
-        button.setOnClickListener {
+    MaterialAlertDialogBuilder(context)
+        .setTitle(context.getString(R.string.update_category))
+        .setView(promptsView)
+        .setPositiveButton(R.string.update) { _, _ ->
             val categoryName = input.text.toString().trim()
             if (categoryName != "") {
                 if (categoryName != catName) {
                     dbManager.update(id, categoryName)
                     val mArrayList = dbManager.getCategoryList()
                     categoryUpdate.isCategoryUpdated(mArrayList)
-                    alertDialog.dismiss()
                 } else {
                     toastMessage(
                         context,
@@ -122,9 +113,8 @@ fun dialogUpdateCategory(context: Context, id: Int, categoryUpdate: CategoryUpda
                 toastMessage(context, context.getString(R.string.please_enter_something_to_update))
             }
         }
-    }
-
-    alertDialog.show()
+        .setNegativeButton(R.string.cancel) { _, _ -> }
+        .show()
 }
 
 /**
@@ -132,25 +122,16 @@ fun dialogUpdateCategory(context: Context, id: Int, categoryUpdate: CategoryUpda
  */
 fun dialogDeleteCategory(context: Context, id: Int, categoryDelete: CategoryDelete) {
     val dbManager = DBManagerCategory(context)
-    val alert = AlertDialog.Builder(context)
-    alert.setTitle(context.getString(R.string.dialog_delete_category_title))
-    alert.setMessage(context.getString(R.string.dialog_delete_category_desc))
-
-    alert.setPositiveButton(R.string.delete) { _, _ -> }
-    alert.setNegativeButton(R.string.cancel) { _, _ -> }
-    val alertDialog = alert.create()
-
-    alertDialog.setOnShowListener {
-        val button = alertDialog.getButton(AlertDialog.BUTTON_POSITIVE)
-        button.setOnClickListener {
+    MaterialAlertDialogBuilder(context)
+        .setTitle(context.getString(R.string.dialog_delete_category_title))
+        .setMessage(context.getString(R.string.dialog_delete_category_desc))
+        .setPositiveButton(R.string.delete) { _, _ ->
             dbManager.delete(id)
             val mArrayList = dbManager.getCategoryList()
             categoryDelete.isCategoryDeleted(mArrayList)
-            alertDialog.dismiss()
         }
-    }
-
-    alertDialog.show()
+        .setNegativeButton(R.string.cancel) { _, _ -> }
+        .show()
 }
 
 fun Int.convertDpToPx(resources: Resources): Int =
